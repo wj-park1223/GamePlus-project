917d0ff745e765bbe821800d1256a3ed862a5c3b
wj-park1223 <pwj021223@naver.com>
2024-10-02 18:28:53 +0900

    git push main

--- src/app/gamelist/page.js
+++ src/app/gamelist/page.js
@@ -2,53 +2,77 @@
 export default function Gamelist(){
     return (
     <div>
-            <div className="game">
-                <h4>배틀그라운드</h4>
-                <p></p>
-            </div>
-            <div className="game">
-                <h4>스타크래프트 </h4>
-                <p>장르 : 실시간 전략 게임</p>
-                <p>개발사 : 블리자드</p>
-            </div>
-            <div className="game">
-                <h4>오버워치 2</h4>
-                <p></p>
-            </div>
-            <div className="game">
-                <h4>이터널 리턴</h4>
-            </div>
-            <div className="game">
-                <h4>디아블로 4</h4>
-            </div>
-            <div className="game">
-                <h4>발로란트</h4>
-            </div>
-            <div className="game">
-                <h4>마인크래프트</h4>
-            </div>
-            <div className="game">
-                <h4>월드 오브 워크래프트</h4>
-            </div>
-            <div className="game">
-                <h4>로스트아크</h4>
-            </div>
-            <div className="game">
-                <h4>원신</h4>
-                <p>장르 : 액션 롤플레잉</p>
-            </div>
-            <div className="game">
-                <h4>블루아카이브</h4>
-            </div>
-            <div className="game">
-                <h4>FC 온라인</h4>
-            </div>
-            <div className="game">
-                <h4>승리의 여신 니케</h4>
-            </div>
-            <div className="game">
-                <h4>포트나이트</h4>
-            </div> 
+        <div className="game">
+            <h4>배틀그라운드</h4>
+            <p>장르 : 배틀로얄</p>
+            <p>개발사 : 크래프톤</p>
+        </div>
+        <div className="game">
+            <h4>스타크래프트</h4>
+            <p>장르 : 실시간 전략</p>
+            <p>개발사 : 블리자드 엔터테인먼트</p>
+        </div>
+        <div className="game">
+            <h4>오버워치 2</h4>
+            <p>장르 : FPS</p>
+            <p>개발사 : 블리자드 엔터테인먼트</p>
+        </div>
+        <div className="game">
+            <h4>이터널 리턴</h4>
+            <p>장르 : 배틀로얄, MOBA</p>
+            <p>개발사 : 님블뉴런</p>
+        </div>
+        <div className="game">
+            <h4>디아블로 4</h4>
+            <p>장르 : 액션 RPG</p>
+            <p>개발사 : 블리자드 엔터테인먼트</p>
+        </div>
+        <div className="game">
+            <h4>발로란트</h4>
+            <p>장르 : FPS</p>
+            <p>개발사 : 라이엇 게임즈</p>
+        </div>
+        <div className="game">
+            <h4>마인크래프트</h4>
+            <p>장르 : 샌드박스</p>
+            <p>개발사 : 모장</p>
+        </div>
+        <div className="game">
+            <h4>월드 오브 워크래프트</h4>
+            <p>장르 : MMORPG</p>
+            <p>개발사 : 블리자드 엔터테인먼트</p>
+        </div>
+        <div className="game">
+            <h4>로스트아크</h4>
+            <p>장르 : MMORPG</p>
+            <p>개발사 : 스마일게이트 RPG</p>
+        </div>
+        <div className="game">
+            <h4>원신</h4>
+            <p>장르 : 액션 RPG</p>
+            <p>개발사 : 미호요</p>
+        </div>
+        <div className="game">
+            <h4>블루아카이브</h4>
+            <p>장르 : RPG</p>
+            <p>개발사 : 넥슨</p>
+        </div>
+        <div className="game">
+            <h4>FC 온라인</h4>
+            <p>장르 : 스포츠</p>
+            <p>개발사 : EA 코리아</p>
+        </div>
+        <div className="game">
+            <h4>승리의 여신 니케</h4>
+            <p>장르 : RPG</p>
+            <p>개발사 : 시프트업</p>
+        </div>
+        <div className="game">
+            <h4>포트나이트</h4>
+            <p>장르 : 배틀로얄</p>
+            <p>개발사 : 에픽 게임즈</p>
+        </div>
+            
     </div>   
     )
 }
