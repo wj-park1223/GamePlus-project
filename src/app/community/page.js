479df06f71bd90a8c3de9a1b68cda0e905eeb2ba
wj-park1223 <pwj021223@naver.com>
2024-10-07 17:22:59 +0900

    git push main

---
+++ src/app/community/page.js
@@ -0,0 +1,30 @@
+import Link from "next/link";
+
+export default function Gamecommunity(){
+    return (
+        <div>
+            <h1>게임라운지</h1>
+            <ui classname ="communityui">
+                <li>네이버 게임라운지</li>
+                <li>스팀 커뮤니티</li>
+                <li>레딧</li>
+            </ui>
+        </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    )
+}
