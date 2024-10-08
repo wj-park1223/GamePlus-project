a404347632c44c30c7c0330b77bef59280718b1a
wj-park1223 <pwj021223@naver.com>
2024-10-08 21:41:50 +0900

    git push main

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -2,24 +2,25 @@ import Link from "next/link";
 
 export default function login(){
     return (
-        <body>
+        <div>
             <div className="gameheader">
                 <h2>로그인</h2>
             </div>
-
-            <div>
-
-
+            
+            <div className="loginbar">
+            <form method="post" action="서버의url" id="login-form">
+            <input type="text" name="userName" placeholder="Email" />
+            <input type="password" name="userPassword" placeholder="Password" />
+            <label htmlFor="remember-check">
+                <input type="checkbox" id="remember-check" />
+                아이디 저장하기
+            </label>
+            <button type="submit">로그인</button>
+            <button type="membership">회원가입</button>
+            </form>
             </div>
-
-
-
-
-
-
-
-
-        </body>
+        
+        </div>
 
 
     )
