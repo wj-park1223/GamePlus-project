755b016dce62d91f66ca81abbe7679d421aaae07
wj-park1223 <pwj021223@naver.com>
2024-10-08 15:31:10 +0900

    git push main

--- src/app/page.js
+++ src/app/page.js
@@ -6,22 +6,22 @@ import Link from "next/link";
 export default function home() {
   return (
     <div>
+
+<div className="search">
+        <input id="search-input" placeholder="검색어를 입력하세요" />
+        <button>검색</button>
+      </div>
+
       <div className="bar">
-          <Link href="/news">게임소식</Link>
-          <Link href="/discount">게임할인</Link>
-      </div>
-      <div className="bar2">       
-          <Link href="/new">신작게임</Link>
-          <Link href="/popular">인기게임</Link>
-      </div>
-       <div className="search">
-          <input id="search-input" placeholder="검색어를 입력하세요" />
-      </div>
+      <div><Link href="/news">게임소식</Link></div>
+        <div><Link href="/discount">게임할인</Link></div>
+         <div><Link href="/new">신작게임</Link></div>
+        <div><Link href="/popular">인기게임</Link></div>
+        </div>
       
+
+
       </div>
   );
 }
-
-
-
 
