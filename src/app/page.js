3ec8deec800e60f1392ab1d23408a72158ca5a8d
wj-park1223 <pwj021223@naver.com>
2024-10-08 23:38:45 +0900

    git push main

--- src/app/page.js
+++ src/app/page.js
@@ -1,27 +1,47 @@
+'use client';
+
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from 'next/navigation';
+import { useState } from 'react';
 
+export default function Home() {
+  const [searchWord, setSearchWord] = useState('');
+  const router = useRouter();
 
+  const showSearchResult = () => {
+    router.push(`https://google.com/search?q=${searchWord}`);
+    setSearchWord('');
+  };
 
-export default function home() {
+  const enterKey = (event) => {
+    if (event.code === 'Enter') {
+      showSearchResult();
+    }
+  };
+
   return (
     <div>
-
-<div className="search">
-        <input id="search-input" placeholder="검색어를 입력하세요" />
-        <button>검색</button>
+      <div className="search">
+        <input
+          id="search-input"
+          value={searchWord}
+          onChange={(e) => setSearchWord(e.target.value)}
+          onKeyDown={enterKey}
+          placeholder="검색어를 입력하세요"
+        />
+        <button onClick={showSearchResult}>검색</button>
       </div>
 
       <div className="bar">
-      <div><Link href="/news">게임소식</Link></div>
+        <div><Link href="/news">게임소식</Link></div>
         <div><Link href="/discount">게임할인</Link></div>
-         <div><Link href="/new">신작게임</Link></div>
+        <div><Link href="/new">신작게임</Link></div>
         <div><Link href="/popular">인기게임</Link></div>
-        </div>
-      
-
-
       </div>
+    </div>
   );
 }
+
+
 
